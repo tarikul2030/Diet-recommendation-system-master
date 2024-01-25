3285d258d2f4f711d213d9679108f3ffe1e52581
tarikul2030 <tarikulislam2030@gmail.com>
2024-01-25 19:58:03 +0600

    Update settings.py

--- foodrec/settings.py
+++ foodrec/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'l#()$jrfbxp-k_9!_^!uz(=9u^q6mfp(=2(m_7pu5xw4v@ou8p'
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['web-production-d5d0.up.railway.app']
+ALLOWED_HOSTS = ['web-production-3aac.up.railway.app']
 
 
 # Application definition
